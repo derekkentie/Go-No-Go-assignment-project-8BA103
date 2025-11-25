64bfc16558f1e1982e09ef8994fc7ad61b6c60ae
Derek <derek.kentie@gmail.com>
2025-11-25 15:33:08 +0100

    data extraction function
    new function added that returns the input variables in as a list of lists in X_train, and returns the output labels in y_train as a list. The integers of the list of variables per sample correspond to the integers of the output labels of each sample.

--- Basis.py
+++ Basis.py
@@ -1,8 +1,30 @@
 import numpy as np
-import pandas
-import matplotlib
+import pandas as pd
+import matplotlib as plt
 
-def train_model(X_train, y_train, **hyperparams):
+# The purpose of the model is to accurately categorise the sleep quality of each sample in the categories: bad, medium and good,
+# based on the variables: Age, sleep duration, study hours, screen time, caffeine intake and physical activity.
+# This means that we 6 variables for the input layer (+1 bias node), and 3 categories for the output layer.
+
+def data_extraction(raw_data):
+    """ This function extracts and returns the X_train and y_train for the train_model function"""
+
+    data = [line.strip().split(',') for line in open(raw_data, 'r')] # extracting all the data from the raw csv file and placing it in a list of lists
+    data.pop(0) #removing the header
+
+    # creating the lists from the input variables and labels
+    X_train = [] 
+    y_train = []
+    for line in data:
+        y_train.append(line.pop(-1))
+        X_train.append(line)
+ 
+    return X_train, y_train
+
+data_extraction("data\\train.csv")
+
+def train_model(X_train, y_train, n_hidden_layers = 6, n_nodes_per_layer = 1, max_iter = 1000):
+    
     """
     Trains your ML algorithm on the provided training data.
 
