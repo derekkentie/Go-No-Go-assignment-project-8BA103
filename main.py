961b6c599306563b461adbe5d9f2d2bf0d0f5507
Derek <derek.kentie@gmail.com>
2025-11-27 11:42:08 +0100

    changes to model and EDUF
    added more information to EDUF about function description. Placed the model in a class, still need to figure out the functions and corresponding parameters

--- main.py
+++ main.py
@@ -1,59 +1,72 @@
 import numpy as np
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # The purpose of the model is to accurately categorise the sleep quality of each sample in the categories: bad, medium and good,
 # based on the variables: Age, sleep duration, study hours, screen time, caffeine intake and physical activity.
 # This means that we 6 variables for the input layer (+1 bias node), and 3 categories for the output layer.
 
-def sigmoid(z):
-    return 1 / (1 + np.exp(-z))
+def sigmoid(z): #sigmoid is used for every node in the hidden layer(s)
+     return 1 / (1 + np.exp(-z))
 
-def data_extraction(raw_data):
-    """ This function extracts and returns the X_train and y_train for the train_model function"""
+def softmax(z): #softmax is used in the output layer to make a enable the model to classify with multiple classes
+    return np.exp(-z) / (1 + np.exp(-z))
 
-    data = [line.strip().split(',') for line in open(raw_data, 'r')] # extracting all the data from the raw csv file and placing it in a list of lists
-    data.pop(0) #removing the header
+def data_extraction_csv(csv_file):
+        """ This function extracts and returns the X_train and y_train for the train_model function"""
+        data = [line.strip().split(',') for line in open(csv_file, 'r')] # extracting all the data from the raw csv file and placing it in a list of lists
+        data.pop(0) #removing the header
+        # creating the lists from the input variables and labels
+        X = []
+        y = []
+        for line in data:
+            y.append(line.pop(-1))
+            X.append(line)
+        # converting the lists to numpy arrays for easier use further on
+        X = np.array(X)
+        y = np.array(y)
+        return X, y
 
-    # creating the lists from the input variables and labels
-    X_train = []
-    y_train = []
-    for line in data:
-        y_train.append(line.pop(-1))
-        X_train.append(line)
-    # converting the lists to numpy arrays for easier use further on
-    X_train = np.array(X_train)
-    y_train = np.array(y_train)
-    return X_train, y_train
+class MultiLayerPerceptron:
+    def _init_(self, n_hidden_layers = 6, n_nodes_per_layer = 1, alpha = 0.1, epochs = 1000):
+        self.theta = None
+        self.n_hidden_layers = n_hidden_layers
+        self.n_nodes_per_layer = n_nodes_per_layer
+        self.alpha = alpha
+        self.epochs = epochs
 
-def train_model(X_train, y_train, n_hidden_layers = 6, n_nodes_per_layer = 1, max_iter = 1000):
-    
-    """
-    Trains your ML algorithm on the provided training data.
+    def fit(self, X, y):
+         pass
 
-    Parameters:
-        X_train (numpy.ndarray): Training features, shape (n_samples, n_features)
-        y_train (numpy.ndarray): Training labels, shape (n_samples,)
-        **hyperparams: Algorithm-specific hyperparameters
-                       (e.g., learning_rate=0.01, max_iter=1000, k=5, etc.)
+    def train_model(X, y, n_hidden_layers = 6, n_nodes_per_layer = 1, epochs = 1000):
+        
 
-    Returns:
-        model_params (dict): A dictionary containing all information needed
-                             to make predictions later.
-                             For example, it might include learned weights,
-                             biases, thresholds, or training statistics.
-    """
-    pass
+        """
+        Trains your ML algorithm on the provided training data.
 
-def predict(X_test, model_params):
-    """
-    Uses the trained parameters to make predictions on new (test) data.
+        Parameters:
+            X_train (numpy.ndarray): Training features, shape (n_samples, n_features)
+            y_train (numpy.ndarray): Training labels, shape (n_samples,)
+            **hyperparams: Algorithm-specific hyperparameters
+                        (e.g., learning_rate=0.01, max_iter=1000, k=5, etc.)
 
-    Parameters:
-        X_test (numpy.ndarray): Test features, shape (n_samples, n_features)
-        model_params (dict): Dictionary of parameters returned by train_model()
+        Returns:
+            model_params (dict): A dictionary containing all information needed
+                                to make predictions later.
+                                For example, it might include learned weights,
+                                biases, thresholds, or training statistics.
+        """
+        pass
 
-    Returns:
-        y_pred (numpy.ndarray): Predicted labels, shape (n_samples,)
-    """
-    pass
+    def predict(X_test, model_params):
+        """
+        Uses the trained parameters to make predictions on new (test) data.
+
+        Parameters:
+            X_test (numpy.ndarray): Test features, shape (n_samples, n_features)
+            model_params (dict): Dictionary of parameters returned by train_model()
+
+        Returns:
+            y_pred (numpy.ndarray): Predicted labels, shape (n_samples,)
+        """
+        pass
